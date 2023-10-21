73ab43e0f76cccb4bdd941e12de26d47de31aa5a
Oleksandr P <goodwin009@gmail.com>
2023-10-21 14:14:57 +0300

    add base structure

--- 01_cli_interactive_sort/app.js
+++ 01_cli_interactive_sort/app.js
@@ -3,3 +3,42 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+const sortingCLI = () => {
+  rl.on('close', () => {
+    console.log('Bye');
+    process.exit(0);
+  });
+  rl.question('Hello', (string) => {
+    const userInput = string.trim().split(' ');
+    console.log('How would ypu like to sort your values');
+    rl.question('Select (1 - 5) and press ENTER: ', (option) => {
+      switch (option) {
+        case '1':
+          console.log('1.Words by name (from A to Z).');
+          break;
+        case '2':
+          console.log('2. Show digits from the smallest.');
+          break;
+        case '3':
+          console.log('3. Show digits from the biggest.');
+          break;
+        case '4':
+          console.log('4. Words by quantity of letters.');
+          break;
+        case '5':
+          console.log('5. Only unique words.');
+          break;
+        case 'exit':
+          console.log('Bye');
+          process.exit(0);
+        default:
+          rl.close();
+          break;
+      }
+      sortingCLI();
+    });
+  });
+};
+
+sortingCLI();
