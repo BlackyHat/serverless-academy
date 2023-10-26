861ae70641ed98c6685781235bbc34ef36a8c878
Oleksandr P <goodwin009@gmail.com>
2023-10-26 10:41:56 +0300

    add base solution 07 without original JSON from web

--- 07_grouping_vacations/app.js
+++ 07_grouping_vacations/app.js
@@ -1,4 +1,4 @@
-const { readFile, writeFile } = require('fs/promises');
+const { readFile } = require('fs/promises');
 const path = require('path');
 
 const dataPath = path.join(__dirname, 'data.json');
@@ -40,5 +40,5 @@ const transformUserData = (data) => {
 
 getUserData().then((res) => {
   const newResponce = transformUserData(res);
-  console.log(newResponce[0]);
+  console.log(newResponce);
 });
